1b1517fe1d197a38e88e3566d07fb909da91e20f
rajinderbarha <rajinderbarha@gmail.com>
2024-02-13 15:14:33 +0530

    created an Homepage Slice via prismic in Next app

---
+++ src/components/Logomove.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Marquee from "react-fast-marquee";
+import Image from 'next/image';
+function Logomove() {
+    return (
+        <div className='Logos_move'>
+            <Marquee className=''>
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos1.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos2.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos3.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos4.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos5.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos6.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos7.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos8.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos9.svg"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    className="w-full mx-auto"
+                    src="/Image/logos_move/logos10.png"
+                    alt="logos"
+                    width={100}
+                    height={100}
+                />
+            </Marquee>
+        </div>
+    )
+}
+
+export default Logomove
---
+++ src/slices/Homepage/index.tsx
@@ -0,0 +1,76 @@
+import Heading from "@/components/Heading";
+import Logomove from "@/components/Logomove";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Homepage`.
+ */
+
+
+const components: JSXMapSerializer = {      //A map of Rich Text block types to React Components. It is used to render Rich Text or Title fields.
+
+  heading1: ({ children }) => (
+    <Heading as="h1" size="xl" className="text-7xl	mb-4 text-center font-semibold">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-lg mb-4 text-center font-semibold">
+      {children}
+    </p>
+  ),
+
+}
+
+
+
+
+export type HomepageProps = SliceComponentProps<Content.HomepageSlice>;
+
+/**
+ * Component for "Homepage" Slices.
+ */
+const Homepage = ({ slice }: HomepageProps): JSX.Element => {
+  return (
+    <>    <section className="Banner"
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+       <div className='benner_Home'>
+        <div className='Beener-bg'>
+          <div className='container_benner'>
+            <div className='Benner_intro'>
+              <PrismicRichText field={slice.primary.banner_heading} components={{
+                heading1: ({ children }) => (
+                  <h1 className="text-7xl	mb-4 text-center font-semibold">{children}</h1>
+                )
+              }} />
+              <PrismicRichText field={slice.primary.banner_paragraph} components={{
+                paragraph: ({ children }) => (
+                  <p className="mx-auto text-lg mb-4 text-center font-medium">{children}</p>
+                )
+              }} /></div>
+            <div className='Benner_img'>
+
+              <PrismicNextImage field={slice.primary.banner_image} className="image_banner mx-auto" />
+
+            </div>
+            <div className='benner_blenk'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+      <section>
+        <Logomove />
+      </section>
+     
+    </>
+
+  );
+};
+
+export default Homepage;
